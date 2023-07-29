43d21fd333e4a071fd101b48728a58d9cbdc5913
Happy Engineer <happyengineer63@gmail.com>
2023-07-30 00:19:48 +0800

    validator (add slug column)

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,12 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class PostsController {
   async index() {
     return await Post.query().preload('category')
   }
   async store({ request }: HttpContextContract) {
-    const data = request.body()
-    return await Post.create(data)
+    const newPostSchema = schema.create({
+      title: schema.string(),
+      categoryId: schema.number.optional(),
+      slug: schema.string([rules.unique({ table: 'posts', column: 'slug' })]),
+    })
+    const payload = await request.validate({ schema: newPostSchema })
+    return await Post.create(payload)
   }
 }
